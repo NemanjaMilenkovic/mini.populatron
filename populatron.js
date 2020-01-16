e5a6420dbc9f4b2dda7f029241d7726e1318b023
NemanjaMilenkovic <nemanja.milenkovic@gmail.com>
2020-01-16 22:47:00 +0900

    Finished

--- populatron.js
+++ populatron.js
@@ -1,5 +1,18 @@
 const fs = require("fs");
 
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation(onFinished) {
+    const getTotalPopulation = async () => {
+      await fs.readFile("cities.csv", "utf8", (err, data) => {
+        if (err) throw err;
+        let sum = 0;
+        let splitFile = data.split("\n").slice(1);
+        for (let line of splitFile) {
+          sum += parseInt(line.split(",")[2]);
+        }
+        onFinished(sum);
+      });
+    };
+    getTotalPopulation();
+  },
 };
